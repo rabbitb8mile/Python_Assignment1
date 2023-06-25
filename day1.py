4a6af49d58aa7d34416cbd131b72cdf5b2945b75
Parth <137251828+rabbitb8mile@users.noreply.github.com>
2023-06-25 11:32:34 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,8 @@
+#print("N word")
+#a=1.0
+#print(type(a))
+num=int(input("enter a num"))
+num2=int(input("enter num2"))
+num3=int(input("enter num3"))
+avg=(num+num2+num3)/3
+print("the average of num1,num2,num3 is ",avg)
